f6f5a250fbd9312712eacfb9e21fc067bed8d08a
ajeddin <abduljeddin@gmail.com>
2022-11-11 23:57:55 +0300

    Home update

--- home.js
+++ home.js
@@ -23,7 +23,7 @@
 
 //CODE HERE
 const greetUser = (username) => {
-    console.log(`Welcome Back ${username}`);
+    console.log(`Welcome Back, ${username}`);
 }
 
 greetUser('Andrew')
@@ -52,29 +52,50 @@ greetUser('Andrew')
 const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
 //CODE HERE
+const canWeDeliver =(zipCode,arr = deliveryAreaZipCodes) => {
+    if (arr.includes(zipCode)) {
+        console.log(`You're in our delivery zone!`); 
+    }else{
+        console.log(`Sorry, we can't deliver to that address`); 
+        
+    }
+}
 
-
-
+canWeDeliver(85203)
 /* 
-    Problem 2 Continued
+Problem 2 Continued
 
-    Now you're going to rewrite your function.
+Now you're going to rewrite your function.
 
-    If you wrote `canWeDeliver` using a loop of
-    some kind, write a new function (`canWeDeliverTwo`)
-    below, using the `includes` array method. 
-    Look it up on MDN if you're not sure how to use 
-    it. 
+If you wrote `canWeDeliver` using a loop of
+some kind, write a new function (`canWeDeliverTwo`)
+below, using the `includes` array method. 
+Look it up on MDN if you're not sure how to use 
+it. 
 
-    If you already used the `includes` method, 
-    write a new function using some sort of 
-    loop (for loop, higher order array method).
-    Name your new function `canWeDeliverTwo`.
+If you already used the `includes` method, 
+write a new function using some sort of 
+loop (for loop, higher order array method).
+Name your new function `canWeDeliverTwo`.
 */
 
 // CODE HERE
+const canWeDeliverTwo= (zipCode,arr=deliveryAreaZipCodes) => {
+    currentZipIteration=arr[0]
 
-
+    for (i=0;i<arr.length;i++){
+        if (currentZipIteration!==(zipCode)) {
+            currentZipIteration=arr[i]
+            
+        }else {
+            return console.log(`You're in our delivery zone!`)
+            
+        }
+        
+        
+    }return console.log(`Sorry, we can't deliver to that address`)
+}
+canWeDeliverTwo(85203)
 //////////////////PROBLEM 3////////////////////
 /* 
     Below is an array of objects that have some
@@ -110,19 +131,41 @@ const deals = [
 
 //CODE HERE
 
+// deal = deals.replace(deals.title ,'10% Off!')
+// console.log(deal);
+dealCopy= deals
+const index = dealCopy.findIndex((el) => el.title === '15% Off!')
+console.log(index);
+// dealCopy[index] = {
+//     title: '10% Off!', 
+//     desc: 'Applied to your entire order when you spend $30 or more'
+// }
 
 
+
+console.log(dealCopy.toString().replace(dealCopy[index].title,  '10% Off!'))
+console.log(dealCopy[index].title);
+// title: '10% Off!', 
+// desc: 'Applied to your entire order when you spend $30 or more'
+
+// console.log(dealCopy);     // bad way remind self to ask about it 
 /*
-    The restaurant is going to continue its
-    family deal for another month. 
+The restaurant is going to continue its
+family deal for another month. 
 
-    Reassign the value of the second deal's desc
-    to be itself, but use the `replace` method
-    to replace the word March with April.
+Reassign the value of the second deal's desc
+to be itself, but use the `replace` method
+to replace the word March with April.
 
-    You should also make sure that there is no
-    whitespace in this string, since it seems
-    to be displaying wrong on the live site.
+You should also make sure that there is no
+whitespace in this string, since it seems
+to be displaying wrong on the live site.
 */
 
 //CODE HERE
+
+// dealCopy.toString().replace(dealCopy.findIndex((el) => el.desc.trim() === 'This deal lasts until the end of March!'),  'This deal lasts until the end of April!')    ASK HOW TO CHAIN TOINDEX AND REPLACE
+// const index2 = dealCopy.findIndex((el) => el.desc.trim() === 'This deal lasts until the end of March!')
+// dealCopy.toString().replace(dealCopy[index2],  'This deal lasts until the end of April!')
+// console.log(dealCopy);   
+// // console.log(index2);
